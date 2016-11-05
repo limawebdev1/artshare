11d8ebcec9e17f28af4453b7d22e27889a55b0cb
Lisa Ma <limawebdev@gmail.com>
2016-11-04 20:04:00 -0600

    migrated and seeded posts

---
+++ migrations/20161104192049_posts.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('posts', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .references('users.id')
+      .onDelete('CASCADE')
+      .notNullable()
+      .index();
+    table.string('title')
+      .notNullable();
+    table.string('description')
+      .notNullable();
+    table.integer('votes')
+      .notNullable();
+    table.string('img')
+      .notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('posts');
+};
