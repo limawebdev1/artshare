0e03be26ddbb58fdfc6b883e40b21904f9f9d227
Lisa Ma <limawebdev@gmail.com>
2016-11-19 23:02:04 -0700

    prepare da heroku

--- server.js
+++ server.js
@@ -62,5 +62,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//for heroku deployment
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+console.log('Listening on port', port);
+});
 
 module.exports = app;
